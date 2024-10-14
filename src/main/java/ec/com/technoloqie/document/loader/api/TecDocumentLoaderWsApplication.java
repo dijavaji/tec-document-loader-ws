70b3d7cc795c5d444f5c72e9a3d36b32648c246e
diegomac <dijavaji@gmail.com>
2024-10-14 12:24:59 -0500

    ADD commit inicial

---
+++ src/main/java/ec/com/technoloqie/document/loader/api/TecDocumentLoaderWsApplication.java
@@ -0,0 +1,13 @@
+package ec.com.technoloqie.document.loader.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecDocumentLoaderWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecDocumentLoaderWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/com/technoloqie/document/loader/api/TecDocumentLoaderWsApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.technoloqie.document.loader.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecDocumentLoaderWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
