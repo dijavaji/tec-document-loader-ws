6da959118a086f6f5f5703069818cd6527f1c8d1
diegomac <dijavaji@gmail.com>
2024-10-25 10:50:34 -0500

    update busqueda de intencion

--- src/main/java/ec/com/technoloqie/document/loader/api/service/IResponseService.java
+++ src/main/java/ec/com/technoloqie/document/loader/api/service/IResponseService.java
@@ -1,9 +1,11 @@
 package ec.com.technoloqie.document.loader.api.service;
 
+import java.util.Collection;
 import java.util.List;
 
 import ec.com.technoloqie.document.loader.api.commons.exception.DocumentLoaderException;
 import ec.com.technoloqie.document.loader.api.dto.ResponseDto;
+import ec.com.technoloqie.document.loader.api.model.Response;
 
 public interface IResponseService {
 	
@@ -12,5 +14,7 @@ public interface IResponseService {
 	void deleteResponse(Integer code)throws DocumentLoaderException;
 	List<ResponseDto> getListResponse()throws DocumentLoaderException;
 	ResponseDto getResponseById(Integer code) throws DocumentLoaderException;
+	
+	void deleteResponseByList(Collection<Response> responses) throws DocumentLoaderException;
 
 }
--- src/main/java/ec/com/technoloqie/document/loader/api/service/impl/PhraseServiceImpl.java
+++ src/main/java/ec/com/technoloqie/document/loader/api/service/impl/PhraseServiceImpl.java
@@ -1,10 +1,12 @@
 package ec.com.technoloqie.document.loader.api.service.impl;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import ec.com.technoloqie.document.loader.api.commons.exception.DocumentLoaderException;
 import ec.com.technoloqie.document.loader.api.dto.PhraseDto;
@@ -22,6 +24,7 @@ public class PhraseServiceImpl implements IPhraseService{
 	private IPhraseRepository phraseRepository;
 	
 	@Override
+	@Transactional
 	public PhraseDto createPhrase(PhraseDto phraseDto) throws DocumentLoaderException {
 		PhraseDto newPhrase = null;
 		try {
@@ -30,12 +33,13 @@ public class PhraseServiceImpl implements IPhraseService{
 			newPhrase = PhraseMapper.mapToPhraseDto(savedPhrase);
 		}catch(Exception e) {
 			log.error("Error al momento de guardar question",e);
-			throw new DocumentLoaderException("Error al momento de guardar question",e);
+			throw new DocumentLoaderException("Error al momento de guardar pregunta",e);
 		}
 		return newPhrase;
 	}
 
 	@Override
+	@Transactional
 	public PhraseDto updatePhrase(PhraseDto phrasedto, int id) throws DocumentLoaderException {
 		Phrase existPhrase = this.phraseRepository.findById(id).orElseThrow(()-> new DocumentLoaderException("Error la pregunta no existe")); //tenemos que comprobar si con la identificación dada existe en la db o no
 		//existAccount.setBalance(intentdto.getBalance());
@@ -47,21 +51,31 @@ public class PhraseServiceImpl implements IPhraseService{
 	}
 
 	@Override
+	@Transactional
 	public void deletePhrase(Integer code) throws DocumentLoaderException {
 		getPhraseById(code);
 		this.phraseRepository.deleteById(code);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<PhraseDto> getListPhrases() throws DocumentLoaderException {
 		List <Phrase> phrases = this.phraseRepository.findAll();
 		return phrases.stream().map((phrase)-> PhraseMapper.mapToPhraseDto(phrase)).collect(Collectors.toList());
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public PhraseDto getPhraseById(Integer code) throws DocumentLoaderException {
 		Phrase phrase = this.phraseRepository.findById(code).orElseThrow(()-> new DocumentLoaderException("Error la pregunta no existe"));
 		return PhraseMapper.mapToPhraseDto(phrase);
+	}
+
+	@Override
+	@Transactional
+	public void deletePhraseByList(Collection<Phrase> phrases) throws DocumentLoaderException {
+		//List <Integer> ids = phrases.stream().map(phrase -> phrase.getId()).collect(Collectors.toList());
+		this.phraseRepository.deleteAll(phrases);
 	}
 
 }
