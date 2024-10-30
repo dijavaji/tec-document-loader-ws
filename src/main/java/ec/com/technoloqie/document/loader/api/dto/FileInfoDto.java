0c24c2fa0cc24fc893b32e9df82ae0296dab2326
diegomac <dijavaji@gmail.com>
2024-10-30 16:40:40 -0500

    agrego servicio de guardado intenciones por archivo

---
+++ src/main/java/ec/com/technoloqie/document/loader/api/dto/FileInfoDto.java
@@ -0,0 +1,17 @@
+package ec.com.technoloqie.document.loader.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class FileInfoDto {
+
+	private String name;
+	private String url;
+
+}
