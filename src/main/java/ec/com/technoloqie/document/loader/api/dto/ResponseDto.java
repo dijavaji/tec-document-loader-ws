7c58aa604d34041a1d7d9b9cee1cea5e5ee549c3
diegomac <dijavaji@gmail.com>
2025-01-17 10:43:41 -0500

    cambio refactor dto no retorna null

--- src/main/java/ec/com/technoloqie/document/loader/api/dto/ResponseDto.java
+++ src/main/java/ec/com/technoloqie/document/loader/api/dto/ResponseDto.java
@@ -1,5 +1,7 @@
 package ec.com.technoloqie.document.loader.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseDto {
 	
 	private int id;
