d85a5ab8e9d7936a7d895e87b63384d9448ef1f7
diegomac <dijavaji@gmail.com>
2025-01-27 19:22:00 -0500

    agrego servicio consulta de archivos por asistente

--- src/main/java/ec/com/technoloqie/document/loader/api/service/IFileService.java
+++ src/main/java/ec/com/technoloqie/document/loader/api/service/IFileService.java
@@ -1,9 +1,13 @@
 package ec.com.technoloqie.document.loader.api.service;
 
+import java.util.List;
+
 import ec.com.technoloqie.document.loader.api.commons.exception.DocumentLoaderException;
 import ec.com.technoloqie.document.loader.api.dto.FileDto;
 
 public interface IFileService {
 	
 	FileDto createFile(String fileName, String fileType, String filePath, Integer assistantId, String createdBy) throws DocumentLoaderException;
+	
+	List<FileDto> getFileByAssistantName(String assistantName) throws DocumentLoaderException;
 }
